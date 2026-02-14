19bd0336e0baa69e95bccdae96d4dd3749065e41
Ash.P <AshbeelPaul@proton.me>
2026-02-14 23:52:09 +0500

    init

---
+++ js/data.js
@@ -0,0 +1,46 @@
+const data = {
+    portfolios: [
+        {
+            techStack: "Premuim, No-Blur",
+            title: "JulietsComms",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/julietscomms.jpg"
+        },
+        {
+            techStack: "Premium, No-Blur",
+            title: "Lucky",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/lucky.jpg"
+        },
+        {
+            techStack: "Premium, Blur",
+            title: "Candy",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/candy.jpg"
+        },
+        {
+            techStack: "Standard, no-Blur",
+            title: "Aria",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/aria.jpg"
+        },
+        {
+            techStack: "Standard, No-Blur",
+            title: "Duckie",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/duckie.jpg"
+        },
+        {
+            techStack: "Standard, No-Blur",
+            title: "Kimchi",
+            githubLink: "#",
+            projectLink: "#",
+            imgLink: "images/portfolio/kimchi.jpg"
+        }
+    ]
+}
